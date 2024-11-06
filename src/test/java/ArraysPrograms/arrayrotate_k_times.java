e1732905d8e3a41f1270bfbed60f258bab241db3
sumitgithub007 <66677424+sumitgithub007@users.noreply.github.com>
2024-11-06 20:39:43 +0530

    Update arrayrotate_k_times.java

--- src/test/java/ArraysPrograms/arrayrotate_k_times.java
+++ src/test/java/ArraysPrograms/arrayrotate_k_times.java
@@ -32,6 +32,58 @@ public class TestClass {
 	 }
 	}
 
+
+	// Java Implementation of Right Rotation 
+// of an Array K number of times
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Array_Rotation
+{
+
+// Function to rightRotate array
+static void RightRotate(int a[], 
+                        int n, int k)
+{
+    
+    // If rotation is greater 
+    // than size of array
+    k=k%n;
+
+    for(int i = 0; i < n; i++)
+    {
+        if(i<k)
+        {
+            // Printing rightmost 
+            // kth elements
+            System.out.print(a[n + i - k] 
+                             + " ");
+        }
+        else
+        {
+            // Prints array after
+            // 'k' elements
+            System.out.print(a[i - k] 
+                             + " ");
+        }
+    }
+    System.out.println();
+}
+    
+// Driver program right rotate
+public static void main(String args[])
+{
+    int Array[] = {1, 2, 3, 4, 5};
+    int N = Array.length;
+
+    int K = 2;
+    RightRotate(Array, N, K);
+
+}
+}
+// This code is contributed by M Vamshi Krishna
+
 	
 	
 		public static void main(String args[])
